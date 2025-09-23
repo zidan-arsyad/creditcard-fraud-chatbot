6eea1b3d74d61ff4972a3bf0c792eafc5e17a2f5
zidan arsyad <deerbomb@gmail.com>
2025-09-24 04:18:56 +0700

    fix tools

--- src/scripts/tools/sql_tool.py
+++ src/scripts/tools/sql_tool.py
@@ -1,14 +1,11 @@
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from scripts.utils.db import get_db
-from scripts.inputs import SQL_TOOL_PROMPT
+from scripts.inputs.prompts_main import SQL_TOOL_PROMPT
 
 
 def get_sql_tool(llm):
     db = get_db()
     sql_toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-    sql_tool = sql_toolkit.get_tools()[0]
-    sql_tool.name = "sql_tool"
-    sql_tool.description = "Tool for accessing credit card users and frauds data."
     return sql_toolkit.get_tools()
 
 
